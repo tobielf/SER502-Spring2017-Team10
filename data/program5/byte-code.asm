fef06b4bf5f317a56a58b555e9244188ea19638f
run-dong-zhu <rundong.zhu@asu.edu>
2017-04-15 23:11:30 -0700

    Update byte-code.asm

---
+++ data/program5/byte-code.asm
@@ -0,0 +1,22 @@
+DEC count
+DEC sum
+MOV count 1
+MOV sum 0
+
+  MOV i count
+    loop1:
+    CMP i 10
+    JE loop1_end
+
+      DEC temp1
+      MOV temp1 i
+      MOD temp1 3
+      CMP temp1 0
+      JE endif1
+
+        ADD sum i
+
+      endif1:
+    ADD i 1
+    loop1_end:
+OUT sum
