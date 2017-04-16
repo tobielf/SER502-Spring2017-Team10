253bdb18210e1f6a382356ff9858f64a386f3363
Xiangyu Guo <tobielf@gmail.com>
2017-04-16 16:15:35 -0700

    Add definitions on symbol table interface.

---
+++ src/compiler/utils/symbol_table.h
@@ -0,0 +1,37 @@
+/**
+ * @file symbol_table.h
+ * @brief Purpose: symbol table data structure
+ * @version 0.1
+ * @date 04.15.2017
+ * @author Xiangyu Guo
+ */
+#ifndef __SYMBOL_TABLE_H__
+#define __SYMBOL_TABLE_H__
+
+typedef struct symbol_table symbol_table_st;
+struct symbol_table;
+
+/**
+ * @brief get one instance of the symbol table.
+ * @return a valid symbol table object.
+ */
+symbol_table_st *symbol_table_get_instance();
+
+/**
+ * @brief look up the symbol on symbol table
+ * @param table a valid symbol table object.
+ * @param symbol a symbol going to look up.
+ * @return -1, not found; otherwise the index in symbol table.
+ */
+int symbol_table_lookup(symbol_table_st *, char *symbol);
+
+/**
+ * @brief insert the symbol to symbol table
+ * @param table a valid symbol table object.
+ * @param symbol a symbol going to insert.
+ * @param token_val the value of the token.
+ * @return -1, failed; otherwise the index inserted in symbol table.
+ */
+int symbol_table_insert(symbol_table_st *, char *symbol, int token_value);
+
+#endif
