3c8f178b633bab457de0ea3e5a9134711ea3afaf
Xiangyu Guo <tobielf@gmail.com>
2017-04-18 01:05:21 -0700

    Removed comma from my byte code.

--- data/program7/byte-code.asm
+++ data/program7/byte-code.asm
@@ -2,12 +2,12 @@ DEC a
 DEC b
 DEC i
 
-MOV a, 0
-MOV b, 1
+MOV a 0
+MOV b 1
 
-MOV i, 1
+MOV i 1
 loop1:
-CMP i, 7
+CMP i 7
 JE loop1_end
     DEC temp
     OUT b
@@ -17,6 +17,6 @@ JE loop1_end
     ADD _temp1 b
     MOV b _temp1
     MOV a temp
-ADD i, 1
+ADD i 1
 JMP loop1
 loop1_end:
