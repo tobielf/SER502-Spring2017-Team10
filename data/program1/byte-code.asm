b462b963e7067be0568e625ee3e4c1f3f148b809
kkmacart <kkmacart@asu.edu>
2017-04-18 13:37:19 -0700

    Update byte-code.asm

--- data/program1/byte-code.asm
+++ data/program1/byte-code.asm
@@ -1,29 +1,31 @@
 DEC x
 DEC i
-MOV i, 1
+MOV i 1
 
 DEC even
 DEC odd
 
 loop:
-CMP x, 25
+CMP x 26
 JE loop_end
     DEC _temp
-    MOV _temp, x
-    MOD _temp, 2
-  CMP _temp, 0
-  JNE endif
+    MOV _temp x
+    MOD _temp 2
+  CMP _temp 0
+  JNE else
         DEC _temp2
-        MOV _temp2, even
-        ADD _temp2, 1
-        MOV even, _temp 2
-  endif:
+        MOV _temp2 even
+        ADD _temp2 1
+        MOV even _temp 2
+        JMP endif
+  else:
         DEC _temp3
-        MOV _temp3, odd
-        ADD _temp3, 1
-        MOV odd, _temp3
-ADD x, 1
-JMP loop
+        MOV _temp3 odd
+        ADD _temp3 1
+        MOV odd _temp3
+  endif:
+        ADD x 1
+        JMP loop
 loop_end:
-OUT even
-OUT odd
+    OUT even
+    OUT odd
