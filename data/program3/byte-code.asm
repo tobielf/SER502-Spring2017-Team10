fee99cd592d278b8e5325cefd5f49e4c392f2ea3
XimingFeng <xfeng36@asu.edu>
2017-04-19 11:36:12 -0700

    modification of the code according to review
    Delete ‘_’ for user defined variable, change description of the test
    case in README

--- data/program3/byte-code.asm
+++ data/program3/byte-code.asm
@@ -1,14 +1,14 @@
-DEC _i
-DEC _num
-MOV _num 0
+DEC i
+DEC num
+MOV num 0
 
-MOV _i 1
+MOV i 1
 loop1:
-	CMP _i 100
+	CMP i 100
 	JE loop1_end
 		if1:
 			DEC _temp1
-			MOV _temp1 _i
+			MOV _temp1 i
 			MOD _temp1 5
 			CMP _temp1 0
 			JNE	if1_end
@@ -16,11 +16,11 @@ loop1:
 		
 		if1_end:
 
-		ADD _i 1
+		ADD i 1
 		JMP loop1
 
 loop1_end:
-	OUT _num
+	OUT num
 
 
 
--- data/program4/byte-code.asm
+++ data/program4/byte-code.asm
@@ -1,23 +1,23 @@
-DEC _i
-DEC _sum
+DEC i
+DEC sum
 
-MOV _i 1
+MOV i 1
 loop1：
-	CMP _i 100
+	CMP i 100
 	JE loop1_end
 	if1:
 		DEC _temp1 
-		MOV _temp1 _i
+		MOV _temp1 i
 		MOD _temp1 2
 		CMP _temp1 0
 		JNE if1_end
 			DEC _temp2
-			MOV _temp2 _i
+			MOV _temp2 i
 			CMP _temp2 0
 			JNE if2_end
-				ADD _sum _i
+				ADD sum i
 		if2_end:
 	if1_end:
 
 loop1_end:
-	OUT _sum
+	OUT sum
