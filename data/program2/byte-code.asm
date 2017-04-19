ac08c372db00c27a2e407eddb6dca35ccf7ed948
kkmacart <kkmacart@asu.edu>
2017-04-19 01:03:10 -0700

    Update byte-code.asm

--- data/program2/byte-code.asm
+++ data/program2/byte-code.asm
@@ -1,31 +1,34 @@
 DEC x
 DEC i
-MOV i, 0
+MOV i 0
 
 loop1:
-CMP x, 100
+CMP x 101
 JE loop1_end
-    DEC _temp
-    MOV _temp, x
-    MOD _temp, 10
-  CMP _temp, 0
-  JNE endif
-    DEC y
-    MOV y, 0
-    loop2:
-    DEC _temp2
-    MOV _temp2, x
-    DIV _temp2, 10
-    CMP y, _temp2
-    JE loop2_end
-        OUT 1
-        ADD y, 1
-        JMP loop2
-    loop2_end:
-    endif:
+    if1:
+        DEC _temp1
+        MOV _temp1 x
+        MOD _temp1 10
+      CMP _temp 0
+      JNE else1
+        DEC y
+        MOV y 0
+        loop2:
+            DEC _temp2
+            MOV _temp2 x
+            DIV _temp2 10
+          CMP y _temp2
+          JE loop2_end
+            OUT 1
+            ADD y 1
+            JMP loop2
+        loop2_end:
+            JMP if1_end
+    else1:  
         OUT 0
-    ADD x, 1
-    JMP loop1
+    if1_end:
+        ADD x 1
+        JMP loop1
 loop1_end
 
 
