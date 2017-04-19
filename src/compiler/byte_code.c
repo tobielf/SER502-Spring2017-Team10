0bd5fa1974e557322e20dd38f71e8f70eeddfe60
run-dong-zhu <rundong.zhu@asu.edu>
2017-04-18 21:37:00 -0700

    Please review my code
    Still not clear with unit test entrance:
    $Q $(CC) -o test_bytecode byte_code.o $(LDFLAGS) $(LDLIBS)
    Should I run this command on cygwin?

---
+++ src/compiler/byte_code.c
@@ -0,0 +1,25 @@
+/**
+ * @file byte_code.c
+ * @brief Purpose: byte code operation
+ * @date 04.18.2017
+ * @author Rundong Zhu
+ */
+
+
+#include "utils/link_list.h"
+#include "utils/symbol_table.h"
+#include "utils/parsing_tree.h"
+
+#include "utils/error.h"
+
+#include "byte_code.h"
+
+/**
+ * @brief generate byte code form parsing tree and symbol table.
+ * @param node, a valid tree node.
+ * @param table, a valid symbol table object
+ * @return NULL on failed, otherwise a valid link list.
+ */
+link_list_st *semantic_analysis(parsing_tree *, symbol_table_st *) {
+  return NULL;
+}
---
+++ src/compiler/byte_code.h
@@ -0,0 +1,25 @@
+/**
+ * @file byte_code.h
+ * @brief Purpose: byte code operation
+ * @date 04.18.2017
+ * @author Rundong Zhu
+ */
+
+#ifndef __BYTE_CODE_H__
+#define __BYTE_CODE_H__
+
+#include "utils/link_list.h"
+#include "utils/symbol_table.h"
+#include "utils/parsing_tree.h"
+
+#include "utils/error.h"
+
+/**
+ * @brief generate byte code form parsing tree and symbol table.
+ * @param node, a valid tree node.
+ * @param table, a valid symbol table object
+ * @return NULL on failed, otherwise a valid link list.
+ */
+link_list_st *semantic_analysis(parsing_tree *, symbol_table_st *);
+
+#endif
