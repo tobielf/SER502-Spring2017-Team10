17670141286d91dff8baaa9ec1b89a687a01f58f
Xiangyu Guo <tobielf@gmail.com>
2017-04-23 11:52:49 -0700

    Refactor the code.
    Take out the storage related operation into a new file.
    ToDo: instruction related work.

--- src/runtime/runtime.c
+++ src/runtime/runtime.c
@@ -1,18 +1,17 @@
 /**
  * @file runtime.c
  * @brief Purpose: main entrance of the runtime
- * @version 0.1
+ * @version 0.5
  * @date 04.16.2017
  * @author Xiangyu Guo
  */
 #include <stdio.h>
 #include <ctype.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define STATIC_MEMORY_SIZE      (32)                /**< memory size of the virtual machine */
-#define SCOPE_BOUNDRAY_SIZE     (4)                 /**< array size of the scope boundray */
-#define RESIZE_FACTOR           (2)                 /**< resize factor when memory size is too small */
+#include "storage.h"
 
 typedef struct label_table
 {
@@ -31,22 +30,9 @@ typedef struct instruction_set
 {
     instruction *instructs;                         /**< all instructions */
     int count;                                      /**< total of instructions */
-} instruction_set_st;
-
-typedef struct memory
-{
-    char *variable_name;                            /**< the name of a variable */
-    int variable_value;                             /**< the value of a variable */
-    int scope;                                      /**< the scope of the variable */
-} memory_st;
-
-typedef struct machine_memory
-{
-    int flag_register;                              /**< flag register for cmp result */
     int program_counter;                            /**< program counter(PC) */
-    int allocated_address;                          /**< usage of static memory */
-    memory_st *static_memory;                       /**< static memory */
-} machine_memory_st;
+
+} instruction_set_st;
 
 typedef void (*eval)(void *, void *);               /**< function pointer of eval functions */
 
@@ -55,10 +41,14 @@ typedef struct operation {
     eval eval_func;                                 /**< evaluate funcion on this operation code */
 } operation_st;
 
-static int s_scope = 0;                             /**< the scope of variable */
+static machine_memory_st *s_machine_store;
 
-static int *s_scope_boundary = NULL;                /**< indicate the scope range */
-
+/**
+ * @brief evaluate function of all binary operations
+ * @param first_operand, first operand.
+ * @param second_operand, second operand.
+ * @param op_type operation type.(+ - * / % =)
+ */
 static void eval_bin_op_helper(void *, void *, char);
 
 /**
@@ -130,6 +120,10 @@ static void eval_mod(void *, void *);
  * MUL var1, var2/value        ; mul var2/value to var1
  * DIV var1, var2/value        ; div var2/value from var1
  * MOD var1, var2/value        ; mod var1 with var2/value
+ *
+ *
+ * Type 5: output result
+ * OUT var1/value          ; Output var1/value. 
  */
 static operation_st g_operations[] = { {"DEC", eval_dec}, 
                                        {"MOV", eval_mov},
@@ -160,28 +154,20 @@ static void s_replace_label(instruction_set_st *, label_table_st *);
  * @param instruct_set [in] loaded instruction sequence.
  * @param machine_store [in/out] the storage of the virtual machine.
  */
-static void s_evaluate(instruction_set_st *, machine_memory_st *);
+static void s_evaluate(instruction_set_st *);
 
 int main()
 {
-    machine_memory_st machine_store;
     instruction_set_st *instructions = NULL;
     label_table_st *labels = NULL;
 
-    machine_store.flag_register = 0;
-    machine_store.program_counter = 0;
-    machine_store.static_memory = (memory_st *)malloc(STATIC_MEMORY_SIZE *
-                                                            sizeof(memory_st));
-    memset(machine_store.static_memory, 0, 
-            sizeof(STATIC_MEMORY_SIZE*sizeof(memory_st)));
-
-    s_scope_boundary = (int *)malloc(SCOPE_BOUNDRAY_SIZE);
+    s_machine_store = machine_memory_init();
 
     s_load_program(instructions, labels);
 
     s_replace_label(instructions, labels);
 
-    s_evaluate(instructions, &machine_store);
+    s_evaluate(instructions);
     return 0;
 }
 
@@ -208,7 +194,7 @@ static void s_replace_label(instruction_set_st *instructions, label_table_st *la
  * @param instruct_set [in] loaded instruction sequence.
  * @param machine_store [in/out] the storage of the virtual machine.
  */
-static void s_evaluate(instruction_set_st *instructions, machine_memory_st *machine_storage) {
+static void s_evaluate(instruction_set_st *instructions) {
 
 }
 
@@ -218,14 +204,28 @@ static void s_evaluate(instruction_set_st *instructions, machine_memory_st *mach
  * @param second second operand.
  */
 static void eval_dec(void *first_operand, void *second_operand) {
+    memory_st *variable_memory;
     char *variable_name = NULL;
+
     if (first_operand == NULL)
         return;
 
     variable_name = (char *)first_operand;
     // check on this scope, if exist warning redefine and exit.
+    variable_memory = machine_memory_get_variable(s_machine_store, 
+                                                    variable_name,
+                                                    MEMORY_CURRENT_SCOPE);
+    if (variable_memory != NULL) {
+        fprintf(stderr, "Redeclare variable: %s exit\n", variable_name);
+        exit(EINVAL);
+    }
 
     // doesn't exist, create a new one.
+    if (machine_memory_set_variable(s_machine_store, variable_name, 
+                                    0, MEMORY_CURRENT_SCOPE) != 0) {
+        fprintf(stderr, "Can't not create new variable.\n");
+        exit(ENOMEM);
+    }
 
 }
 
@@ -235,6 +235,7 @@ static void eval_dec(void *first_operand, void *second_operand) {
  * @param second second operand.
  */
 static void eval_out(void *first_operand, void *second_operand) {
+    memory_st *variable_memory;
     char *var_one = NULL;
     int value = 0;
 
@@ -245,7 +246,15 @@ static void eval_out(void *first_operand, void *second_operand) {
 
     if (isalpha(var_one[0])) {
         // check var_one on ALL scope, if doesn't exist, warning and exit.
+        variable_memory = machine_memory_get_variable(s_machine_store, 
+                                                      var_one, 
+                                                      MEMORY_ALL_SCOPE);
+        if (variable_memory == NULL) {
+            fprintf(stderr, "Using undeclared variables! Exit\n");
+            exit(EINVAL);
+        }
         // value 
+        value = memory_get_value(variable_memory);
     } else {
         value = atoi(var_one);
     }
@@ -307,7 +316,17 @@ static void eval_mod(void *first_operand, void *second_operand) {
     eval_bin_op_helper(first_operand, second_operand, '%');
 }
 
-static void eval_bin_op_helper(void *first_operand, void *second_operand, char op) {
+/**
+ * @brief evaluate function of all binary operations
+ * @param first_operand, first operand.
+ * @param second_operand, second operand.
+ * @param op_type operation type.(+ - * / % =)
+ */
+static void eval_bin_op_helper(void *first_operand, 
+                               void *second_operand, 
+                               char op_type) {
+    memory_st *var_mem_one;
+    memory_st *var_mem_two;
     char *var_one = NULL;
     char *var_two = NULL;
     int value_one = 0;
@@ -320,34 +339,55 @@ static void eval_bin_op_helper(void *first_operand, void *second_operand, char o
     var_two = (char *)second_operand;
 
     // check var_one on ALL scope, if doesn't exist, warning and exit.
-
+    var_mem_one = machine_memory_get_variable(s_machine_store, 
+                                              var_one, 
+                                              MEMORY_ALL_SCOPE);
+    if (var_mem_one == NULL) {
+        fprintf(stderr, "Using undeclared variables %s! Exit\n", var_one);
+        exit(EINVAL);
+    }
     // value_one = from var_one on the storage.
+    value_one = memory_get_value(var_mem_one);
 
     if (isalpha(var_two[0])) {
         // check var_two on ALL scope, if doesn't exist, warning and exit.
-
+        var_mem_two = machine_memory_get_variable(s_machine_store, 
+                                                  var_two, 
+                                                  MEMORY_ALL_SCOPE);
+        if (var_mem_two == NULL) {
+            fprintf(stderr, "Using undeclared variables %s! Exit\n", var_one);
+            exit(EINVAL);
+        }
         // value_two = from var_two on the storage.
+        value_two = memory_get_value(var_mem_two);
     } else {
         value_two = atoi(second_operand);
     }
  
-    switch (op) {
+    switch (op_type) {
         case '+':
+            value_one += value_two;
             break;
         case '-':
+            value_one -= value_two;
             break;
         case '*':
+            value_one *= value_two;
             break;
         case '/':
+            value_one /= value_two;
             break;
         case '%':
+            value_one %= value_two;
             break;
         case '=':
+            value_one = value_two;
             break;
         default:
-            exit(-1);
+            exit(EPERM);
             break;
     }
     
     // put value into the var_one.
+    memory_set_value(var_mem_one, value_one);
 }
